bf28e13bc32d5b59feb3679f34391a64da8c3b8a
suuzanemoura <suuzanemoura@gmail.com>
2025-08-07 19:32:57 -0300

    Implementa efeito de entrada com jQuery e delay em cascata

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-// main.js - Inicialmente vazio para testar apenas o layout estático
-// Use este arquivo para implementar animações JS depois!
+//Efeito de exibição inicial dos cards (efeito cascata) - jQuery
+
+$(document).ready(function () {
+  const cards = $(".card");
+
+  // Esconde todos os cards inicialmente
+  cards.removeClass("show");
+
+  // Exibe os cards com um efeito de cascata
+
+  for (let i = 0; i < cards.length; i++) {
+    setTimeout(() => {
+      $(cards[i]).addClass("show");
+    }, i * 500); // delay de 500ms entre cada card
+  }
+});
